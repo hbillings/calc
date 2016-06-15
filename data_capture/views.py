93518d35816d1a0105864a9f4d40829bf28994e6
James Seppi <james.seppi@gmail.com>
2016-06-15 08:21:27 -0500

    minimal error handling for bad uploads

--- data_capture/views.py
+++ data_capture/views.py
@@ -14,31 +14,48 @@ class LoadDataView(FormView):
     form_class = LoadDataForm
     success_url = "/data_capture/"
 
+    def clear_contracts(self):
+        management.call_command('flush', verbosity=0, interactive=False)
+
+    def load_contracts(self, uploaded_file):
+        contracts = list(ContractDataReader().parse(
+            codecs.iterdecode(uploaded_file, 'utf-8')))
+
+        Contract.objects.all().delete()
+
+        Contract.objects.bulk_create(contracts)
+
+        management.call_command(
+            'update_search_field',
+            Contract._meta.app_label, Contract._meta.model_name
+        )
+        return contracts
+
     def form_valid(self, form):
-        if 'load' in form.data and 'file' in self.request.FILES:
-            uploaded_file = self.request.FILES['file']
-            contracts = list(ContractDataReader().parse(
-                codecs.iterdecode(uploaded_file, 'utf-8')))
+        if 'load' in form.data:
+            if 'file' not in self.request.FILES:
+                messages.add_message(
+                    self.request, messages.ERROR, 'File must be selected')
+            else:
+                uploaded_file = self.request.FILES['file']
 
-            Contract.objects.all().delete()
+                try:
+                    contracts = self.load_contracts(uploaded_file)
+                    messages.add_message(
+                        self.request, messages.SUCCESS,
+                        'Loaded %d contract records' % len(contracts)
+                    )
+                except:
+                    messages.add_message(
+                        self.request, messages.ERROR,
+                        'Unable to read contracts from uploaded file'
+                    )
+                    print("here")
 
-            Contract.objects.bulk_create(contracts)
-
-            management.call_command(
-                'update_search_field',
-                Contract._meta.app_label, Contract._meta.model_name
-            )
-
-            messages.add_message(
-                self.request, messages.SUCCESS,
-                'Loaded %d contract records' % len(contracts)
-            )
         elif 'clear' in form.data:
-            management.call_command('flush', verbosity=0, interactive=False)
+            self.clear_contracts()
             messages.add_message(
                 self.request, messages.SUCCESS, 'Data cleared'
             )
-        else:
-            messages.add_message(
-                self.request, messages.ERROR, 'File must be selected')
+
         return super(LoadDataView, self).form_valid(form)
