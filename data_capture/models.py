d4977ae0ac72a1961c52e50607449f9e0e2ab684
James Seppi <james.seppi@gmail.com>
2016-06-21 09:31:12 -0500

    add enumerated choices, rename PriceListRow, addl help_text

--- data_capture/models.py
+++ data_capture/models.py
@@ -2,6 +2,18 @@ from django.db import models
 
 from contracts.models import EDUCATION_CHOICES
 
+DOMESTICITY_CHOICES = (
+    ('D', 'Domestic Only'),
+    ('O', 'Overseas Only'),
+    ('W', 'Worldwide')
+)
+
+SITE_CHOICES = (
+    ('CON', 'Contractor Site'),
+    ('CUS', 'Customer Site'),
+    ('BOTH', 'Both')
+)
+
 
 # PriceList is a model for storing the data when a price list XLS is uploaded
 # A PriceList has many PriceRows
@@ -33,19 +45,33 @@ class PriceList(models.Model):
 # It should be trimmed down to only the necessary fields, possibly those
 # described by FLIFCALC.
 # https://github.com/18F/calc/wiki/Federal-Labor-Interchange-Format-for-CALC
-class PriceRow(models.Model):
+class PriceListRow(models.Model):
     sin = models.CharField(
-        help_text="Proposed SIN(s)"  # TODO: What does SIN stand for?
+        help_text="Proposed Special Item Numbers (SINs)"
     )
+
     labor_category = models.CharField(
         help_text="Labor Category"
     )
+
     education_level = models.CharField(
         db_index=True, choices=EDUCATION_CHOICES, max_length=5,
         null=True, blank=True, help_text="Minimum Education Level")
-    minimum_education_years = models.IntegerField()
-    contractor_site = models.CharField()
-    domesticity = models.CharField()  # domestic only, overseas only, worldwide
+
+    minimum_education_years = models.IntegerField(
+        help_text="Minimum Years of Education"
+    )
+
+    contractor_site = models.CharField(
+        choices=SITE_CHOICES,
+        help_text="Customer Site, Contractor Site, or Both"
+    )
+
+    # TODO: This might not be necessary if CALC is only supposed to show
+    # worldride rates
+    domesticity = models.CharField(
+        choices=DOMESTICITY_CHOICES,
+        help_text="Domestic, Overseas, or Worldwide")
 
     # Not in the example XLS, but perhaps should be as it would capture
     # the yearly percentage increase of the negotiated rate
@@ -63,7 +89,6 @@ class PriceRow(models.Model):
     mfc_discount = models.DecimalField(
         help_text="Most Favored Customer Discount (Percent)"
     )
-
     mfc_price_per_hour = models.DecimalField(
         help_text="Most Favored Customer Price per Hour"
     )
@@ -84,5 +109,5 @@ class PriceRow(models.Model):
         help_text="Support Document Number"
     )
 
-    # PriceList has many PriceRows
+    # PriceList has many PriceListRows
     price_list = models.ForeignKey(PriceList, on_delete=models.CASCADE)
